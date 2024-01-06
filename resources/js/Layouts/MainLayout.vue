91a0f7053f1925ccd242e47ea90c38084e1d49d7
Laurin49 <welter@diwe.info>
2024-01-06 13:06:23 +0100

    dev00 initial setup

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="w-full bg-white border-b border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+    <div class="container mx-auto">
+      <nav class="flex items-center justify-between p-4">
+        <div class="text-xl font-bold text-center text-indigo-600 dark:text-indigo-300">
+          <Link :href="route('index')">Home</Link>
+        </div>
+        <div class="text-xl font-bold text-center text-indigo-600 dark:text-indigo-300">
+          <Link :href="route('index.show')">Show</Link>
+        </div>
+        <div>
+          <Link :href="route('index.create')" class="btn-primary">+ Create</Link>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <main class="container p-4 mx-auto">
+    <slot>Default</slot>
+  </main>
+</template>
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+
+</script>
---
+++ resources/js/Pages/Index/Create.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>Create</div>
+</template>
+<script setup>
+
+</script>
